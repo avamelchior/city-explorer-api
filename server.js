d7fe3880f239efc2be0e831b4130d7bdfeeb0d1b
tektechnologies <info@tektechnologies.com>
2023-10-25 09:39:59 -0500

    server sending weather data

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 require('dotenv').config();
 let data = require('./data/weather.json');
-console.log(data);
+// console.log('!!!!!!!!',data);
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5005;
 app.use(cors());
 
 app.get('/weather', (req, res, next) => {
   try {
-    console.log(req.query);
+    // console.log('!!!!!',req.query);
     let searchQuery = req.query.searchQuery;
-    let filteredData = data.filter(weather => weather.city === searchQuery);
-    const forecasts = filteredData.map(weatherDataToInstant => new Forecast(weatherDataToInstant));
-    res.json({ forecasts });
+    let filteredData = data.filter(weather => weather.city_name.toLowerCase() === searchQuery.toLowerCase());
+    // console.log('0000000',filteredData);
+    // const forecasts = filteredData[0].data.map(weatherDataToInstant => console.log('TTT', weatherDataToInstant));
+
+    const forecasts = filteredData[0].data.map(weatherDataToInstant => new Forecast(weatherDataToInstant));
+    console.log('forecast!!!!', forecasts);
+    res.status(200).send(forecasts);
   } catch (error) {
     next(error);
   }
@@ -22,8 +26,9 @@ app.get('/weather', (req, res, next) => {
 
 class Forecast {
   constructor(weatherObject) {
+    // console.log('weather!!!!!!',weatherObject);
     this.date = weatherObject.datetime;
-    this.description = weatherObject.description;
+    this.description = weatherObject.weather.description;
   }
 }
 
