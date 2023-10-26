92691a59b092b424afba9dbb19f04b687af64d29
Ava Melchior <avacmelchior@gmail.com>
2023-10-25 19:28:32 -0500

    lab 7

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
 const express = require('express');
 require('dotenv').config();
-let data = require('./data/weather.json');
-// console.log('!!!!!!!!',data);
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5005;
 app.use(cors());
 
-app.get('/weather', (req, res, next) => {
-  try {
-    // console.log('!!!!!',req.query);
-    let searchQuery = req.query.searchQuery;
-    let filteredData = data.filter(weather => weather.city_name.toLowerCase() === searchQuery.toLowerCase());
-    // console.log('0000000',filteredData);
-    // const forecasts = filteredData[0].data.map(weatherDataToInstant => console.log('TTT', weatherDataToInstant));
+const weatherApiKey = process.env.WEATHER_API_KEY;
 
-    const forecasts = filteredData[0].data.map(weatherDataToInstant => new Forecast(weatherDataToInstant));
-    console.log('forecast!!!!', forecasts);
-    res.status(200).send(forecasts);
+app.get('/weather', async (req, res, next) => {
+  try {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    if (!lat || !lon) {
+      return res.status(400).json({ error: 'Latitude and longitude are required in the request' });
+    }
+
+    const response = await axios.get(`https://api.example.com/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`);
+
+    const weatherData = response.data;
+
+    const forecasts = weatherData.daily.map(weatherDataToInstant => new Forecast(weatherDataToInstant));
+
+    res.status(200).json(weatherData);
   } catch (error) {
     next(error);
   }
 });
-
-class Forecast {
-  constructor(weatherObject) {
-    // console.log('weather!!!!!!',weatherObject);
-    this.date = weatherObject.datetime;
-    this.description = weatherObject.weather.description;
-  }
-}
 
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
 
 app.get('*', (request, response) => {
-  response.send('the route does not exsist, sorry. ERROR 404');
+  response.send('the route does not exist, sorry. ERROR 404');
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
 
